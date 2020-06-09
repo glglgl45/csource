533944da293fe3b1967caca6a83a102edf7edde3
정환 윤 <glglgl47@naver.com>
2020-06-09 09:37:19 +0900

    06090937

---
+++ construct/construct/construct.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Dataprocess {
+public:
+	Dataprocess() {
+		digit1 = 10;
+	}
+	int getData_info() const;
+	void setData_info() const;
+private:
+	int digit1;
+};
+int Dataprocess::getData_info() const {
+	setData_info();
+	return digit1;
+}
+void Dataprocess::setData_info() const {
+	cout << "digit1의 출력결과" << endl;
+}
+int main() {
+	Dataprocess dsample;
+	cout << "계산결과" << dsample.getData_info() << endl;
+}
---
+++ enum/enum/enum.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+// 열거형(enumeration)
+// 이름 있는 정수형 상수들을 열거하여 새롭게 만들어진 사용자 정의
+// 열거형 목록값은 프로그래머가 특별히 지정하지 않으면 0부터 시작
+
+enum colors{black,white,red,green,yellow};
+
+int main() {
+	colors mycolor;
+	mycolor = black;
+	const char *meaning[] = { "어둠","평화","정열","생명","순수" };
+	cout << "당신이 선택한 색상의 의미 : " << meaning[mycolor] << 
+}
